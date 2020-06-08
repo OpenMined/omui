24a7864e9d28d2535149957bb875065a1ba0823f
Patrick Cason <me@patrickcason.com>
2020-06-08 21:06:15 +0100

    Cannot get accordion sections to work, stopping for the night

--- omui.framerfx/utils/theme-hoc.tsx
+++ omui.framerfx/utils/theme-hoc.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ThemeProvider, CSSReset } from '../../omui';
 import theme from '../../omui/theme';
 
-export const themeHOC = (Component) => (props) => (
+export const themeHOC = (Component: any) => (props: any) => (
   <ThemeProvider theme={theme}>
     <CSSReset />
     <Component {...props} />
