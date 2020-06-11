4f33564ec0075376b22fc382c38c76f76eab00d4
Thiago Costa Porto <thiagocostaporto@gmail.com>
2020-06-11 04:25:22 +0100

    Support for ts in storybook

--- omui/.storybook/main.js
+++ omui/.storybook/main.js
@@ -1,8 +1,20 @@
 module.exports = {
   addons: [
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-storysource',
-    '@storybook/addon-viewport/register',
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport/register",
   ],
-  stories: ['../stories/**/*.stories.js'],
+  stories: ["../stories/**/*.stories.js", "../stories/**/*.stories.jsx"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("ts-loader"),
+        },
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
 };
