70fd26e8abe4661d615fcccd6665fee72dbcb180
Patrick Cason <me@patrickcason.com>
2020-07-22 17:18:41 +0100

    Final colors from design team and some docs changes

--- omui/src/theme/foundations/colors.ts
+++ omui/src/theme/foundations/colors.ts
@@ -60,42 +60,55 @@ export const colors = {
     800: '#323232', // Original: #1A202C
     900: '#202020' // Original: #171923
   },
+  magenta: {
+    // Original: pink
+    50: '#F6E3EC', // Original: #fff5f7
+    100: '#E9B9D0', // Original: #fed7e2
+    200: '#DD8CB2', // Original: #fbb6ce
+    300: '#D26194', // Original: #f687b3
+    400: '#CA437E', // Original: #ed64a6
+    500: '#BD2C6A', // Original: #d53f8c
+    600: '#AA235E', // Original: #b83280
+    700: '#952558', // Original: #97266d
+    800: '#7C1E4E', // Original: #702459
+    900: '#641947' // Original: #521B41
+  },
   red: {
-    50: '#FCE9EE', // Original: #fff5f5
-    100: '#F7C7D1', // Original: #fed7d7
-    200: '#E48F99', // Original: #feb2b2
-    300: '#D66373', // Original: #fc8181
-    400: '#DF3A53', // Original: #f56565
-    500: '#E3173B', // Original: #e53e3e
-    600: '#D40639', // Original: #c53030
-    700: '#C30033', // Original: #9b2c2c
-    800: '#B6002B', // Original: #822727
-    900: '#A70020' // Original: #63171b
+    50: '#FFE5EC', // Original: #fff5f5
+    100: '#FCC4D0', // Original: #fed7d7
+    200: '#EA8396', // Original: #feb2b2
+    300: '#DC6173', // Original: #fc8181
+    400: '#D45060', // Original: #f56565
+    500: '#C12A45', // Original: #e53e3e
+    600: '#B10C33', // Original: #c53030
+    700: '#A30B2C', // Original: #9b2c2c
+    800: '#920B28', // Original: #822727
+    900: '#7A051E' // Original: #63171b
   },
   orange: {
-    50: '#FCF0DE', // Original: #FFFAF0
-    100: '#F7D8AE', // Original: #FEEBC8
-    200: '#F1BF7A', // Original: #FBD38D
-    300: '#ECA645', // Original: #F6AD55
-    400: '#E8941B', // Original: #ED8936
-    500: '#E58200', // Original: #DD6B20
-    600: '#E17700', // Original: #C05621
-    700: '#DB6800', // Original: #9C4221
-    800: '#D55800', // Original: #7B341E
-    900: '#CB3C00' // Original: #652B19
+    50: '#FCEEDE', // Original: #FFFAF0
+    100: '#F7D3AD', // Original: #FEEBC8
+    200: '#F1B77A', // Original: #FBD38D
+    300: '#EB9A47', // Original: #F6AD55
+    400: '#E88522', // Original: #ED8936
+    500: '#E27101', // Original: #DD6B20
+    600: '#E06113', // Original: #C05621
+    700: '#D14D02', // Original: #9C4221
+    800: '#CF3C00', // Original: #7B341E
+    900: '#C6260A' // Original: #652B19
   },
-  // yellow: {
-  //   50: '#fffff0',
-  //   100: '#fefcbf',
-  //   200: '#faf089',
-  //   300: '#f6e05e',
-  //   400: '#ecc94b',
-  //   500: '#d69e2e',
-  //   600: '#b7791f',
-  //   700: '#975a16',
-  //   800: '#744210',
-  //   900: '#5F370E'
-  // },
+  yellow: {
+    50: '#FEF7DE', // Original: #fffff0
+    100: '#FCEAAB', // Original: #fefcbf
+    200: '#FADD73', // Original: #faf089
+    300: '#F7D136', // Original: #f6e05e
+    400: '#EBBD0A', // Original: #ecc94b
+    500: '#D7A30A', // Original: #d69e2e
+    600: '#B97F0C', // Original: #b7791f
+    700: '#97600D', // Original: #975a16
+    800: '#82500A', // Original: #744210
+    900: '#734301' // Original: #5F370E
+  },
   green: {
     50: '#E6F3E6', // Original: #f0fff4
     100: '#C2E0C1', // Original: #c6f6d5
@@ -103,71 +116,72 @@ export const colors = {
     300: '#73B973', // Original: #68d391
     400: '#56AB56', // Original: #48bb78
     500: '#399D3A', // Original: #38a169
-    600: '#308F31', // Original: #2f855a
-    700: '#257D27', // Original: #276749
-    800: '#1A6D1E', // Original: #22543d
+    600: '#308B31', // Original: #2f855a
+    700: '#267D28', // Original: #276749
+    800: '#267D28', // Original: #22543d
     900: '#034F0C' // Original: #1C4532
   },
   teal: {
-    50: '#E1F2EF', // Original: #E6FFFA
-    100: '#B6DFD7', // Original: #B2F5EA
-    200: '#88CABE', // Original: #81E6D9
-    300: '#5CB5A4', // Original: #4FD1C5
-    400: '#41A592', // Original: #38B2AC
-    500: '#2F9580', // Original: #319795
-    600: '#2B8874', // Original: #2C7A7B
-    700: '#267865', // Original: #285E61
-    800: '#216857', // Original: #234E52
-    900: '#184C3C' // Original: #1D4044
+    50: '#DFF1F1', // Original: #E6FFFA
+    100: '#AEDEDC', // Original: #B2F5EA
+    200: '#79C9C5', // Original: #81E6D9
+    300: '#3EB3AD', // Original: #4FD1C5
+    400: '#00A39B', // Original: #38B2AC
+    500: '#009289', // Original: #319795
+    600: '#00837C', // Original: #2C7A7B
+    700: '#006F6A', // Original: #285E61
+    800: '#005C59', // Original: #234E52
+    900: '#004A48' // Original: #1D4044
   },
   cyan: {
-    50: '#E3F6F8', // Original: #EDFDFD
-    100: '#BBE8EC', // Original: #C4F1F9
-    200: '#95DAE0', // Original: #9DECF9
-    300: '#77CAD3', // Original: #76E4F7
-    400: '#6BBFC9', // Original: #0BC5EA
-    500: '#69B4C0', // Original: #00B5D8
-    600: '#62A4AE', // Original: #00A3C4
-    700: '#5A8F96', // Original: #0987A0
-    800: '#537B7F', // Original: #086F83
-    900: '#475757' // Original: #065666
+    50: '#D8F6FA', // Original: #EDFDFD
+    100: '#D8F6FA', // Original: #C4F1F9
+    200: '#7CD8E4', // Original: #9DECF9
+    300: '#50CFE0', // Original: #76E4F7
+    400: '#0FC2DA', // Original: #0BC5EA
+    500: '#00AFC9', // Original: #00B5D8
+    600: '#00A2B7', // Original: #00A3C4
+    700: '#008EA3', // Original: #0987A0
+    800: '#007485', // Original: #086F83
+    900: '#00525B' // Original: #065666
   },
   blue: {
-    50: '#E7E8EE', // Original: #ebf8ff
-    100: '#C2C6D7', // Original: #ceedff
-    200: '#9BA2BC', // Original: #90cdf4
-    300: '#767EA1', // Original: #63b3ed
-    400: '#5B638F', // Original: #4299e1
-    500: '#3D4984', // Original: #3182ce
-    600: '#37427C', // Original: #2a69ac
-    700: '#2F3971', // Original: #1e4e8c
-    800: '#272F66', // Original: #153e75
-    900: '#181F54' // Original: #1a365d
+    50: '#E4EFF5', // Original: #ebf8ff
+    100: '#BCD8E7', // Original: #ceedff
+    200: '#96C0D7', // Original: #90cdf4
+    300: '#75A7C6', // Original: #63b3ed
+    400: '#578CB1', // Original: #4299e1
+    500: '#427FAB', // Original: #3182ce
+    600: '#3470A0', // Original: #2a69ac
+    700: '#346692', // Original: #1e4e8c
+    800: '#245382', // Original: #153e75
+    900: '#223F64' // Original: #1a365d
   },
-  purple: {
+  indigo: {
+    50: '#E7E8EE',
+    100: '#C2C6D7',
+    200: '#9BA2BC',
+    300: '#767EA1',
+    400: '#5B638F',
+    500: '#49558E',
+    600: '#3A4583',
+    700: '#2F3971',
+    800: '#212962',
+    900: '#181F54'
+  },
+  violet: {
+    // Original: purple
     50: '#F1E3EF', // Original: #faf5ff
     100: '#DCBAD7', // Original: #e9d8fd
     200: '#C68DBE', // Original: #d6bcfa
     300: '#AE62A4', // Original: #b794f4
     400: '#9D4392', // Original: #9f7aea
     500: '#8E2183', // Original: #805ad5
-    600: '#821D7E', // Original: #6b46c1
-    700: '#721776', // Original: #553c9a
-    800: '#64116E', // Original: #44337a
-    900: '#4B0661' // Original: #322659
+    600: '#7C0C78', // Original: #6b46c1
+    700: '#650869', // Original: #553c9a
+    800: '#530765', // Original: #44337a
+    900: '#430258' // Original: #322659
   }
-  // pink: {
-  //   50: '#fff5f7',
-  //   100: '#fed7e2',
-  //   200: '#fbb6ce',
-  //   300: '#f687b3',
-  //   400: '#ed64a6',
-  //   500: '#d53f8c',
-  //   600: '#b83280',
-  //   700: '#97266d',
-  //   800: '#702459',
-  //   900: '#521B41'
-  // }
 };
 
 export const thirdParty = {
