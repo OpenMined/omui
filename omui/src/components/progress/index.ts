95320722f7a7d0a4b7fa3bb1cc49e53b7678d070
Héricles <hericles.me@gmail.com>
2020-07-24 14:02:12 -0300

    update select styles

--- omui/src/components/progress/index.ts
+++ omui/src/components/progress/index.ts
@@ -1,2 +1,2 @@
 export { LinearProgress, CircularProgress } from './progress';
-export { default as ProgressStyle, ProgressSizes } from './progress.style';
+export { default as ProgressStyle } from './progress.style';
--- omui/src/components/select/select.style.ts
+++ omui/src/components/select/select.style.ts
@@ -1,20 +1,5 @@
-// import { ComponentTheme } from '@chakra-ui/theme-tools';
+import theme from '@chakra-ui/theme';
 
-import { default as InputStyle } from '../input/input.style';
-
-// TODO: remove the 'any' type
-const Select: any = {
-  ...InputStyle,
-  baseStyle: {
-    ...InputStyle.baseStyle,
-    appearance: 'none',
-    paddingRight: '2rem',
-    paddingBottom: '1px',
-    lineHeight: 'normal'
-  }
-};
-
-export const SelectSizes = InputStyle.sizes;
-export const SelectVariants = InputStyle.variants;
+const { Select } = theme.components;
 
 export default Select;
