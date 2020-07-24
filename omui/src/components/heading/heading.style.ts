ec511ba18d64603351e778610f6371b644843143
Héricles <hericles.me@gmail.com>
2020-07-24 11:36:04 -0300

    update heading styles

--- omui/src/components/heading/heading.style.ts
+++ omui/src/components/heading/heading.style.ts
@@ -1,19 +1,21 @@
-import { BaseStyle, DefaultProps, Sizes } from '@chakra-ui/theme-tools';
+import theme from '@chakra-ui/theme';
+import { Sizes } from '@chakra-ui/theme-tools';
+
+const { Heading } = theme.components;
 
 const register = {
-  parts: ['heading'],
+  ...Heading.register,
   sizes: ['hero', '2xl', 'xl', 'lg', 'md', 'sm', 'xs']
 } as const;
 
-const baseStyle: BaseStyle<typeof register> = {
-  heading: {
-    fontFamily: 'heading',
-    lineHeight: 'base',
-    fontWeight: 'medium'
-  }
+Heading.baseStyle.heading = {
+  ...Heading.baseStyle.heading,
+  lineHeight: 'base',
+  fontWeight: 'medium'
 };
 
 const sizes: Sizes<typeof register> = {
+  ...Heading.sizes,
   hero: {
     heading: { fontSize: ['5xl', null, '6xl'], lineHeight: 'none' }
   },
@@ -22,24 +24,9 @@ const sizes: Sizes<typeof register> = {
   },
   xl: {
     heading: { fontSize: ['3xl', null, '4xl'], lineHeight: 'short' }
-  },
-  lg: {
-    heading: { fontSize: ['xl', null, '2xl'] }
-  },
-  md: { heading: { fontSize: 'xl' } },
-  sm: { heading: { fontSize: 'md' } },
-  xs: { heading: { fontSize: 'sm' } }
+  }
 };
 
-const defaultProps: DefaultProps<typeof register> = {
-  size: 'xl'
-};
+Heading.sizes = sizes;
 
-const heading = {
-  register,
-  defaultProps,
-  baseStyle,
-  sizes
-};
-
-export default heading;
+export default Heading;
