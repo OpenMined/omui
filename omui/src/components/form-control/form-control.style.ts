910ed13ca0dcfa03d7fefa523ad74669e55e14fb
Héricles <hericles.me@gmail.com>
2020-07-24 11:20:57 -0300

    update form-control styles

--- omui/src/components/form-control/form-control.style.ts
+++ omui/src/components/form-control/form-control.style.ts
@@ -1,47 +1,5 @@
-import { BaseStyle, mode } from '@chakra-ui/theme-tools';
+import theme from '@chakra-ui/theme';
 
-const register = {
-  parts: ['label', 'errorText', 'requiredIndicator', 'helperText', 'errorIcon']
-} as const;
-
-const baseStyle: BaseStyle<typeof register> = (props) => {
-  return {
-    label: {
-      fontSize: 'md',
-      marginRight: 3,
-      marginBottom: 2,
-      fontWeight: 'medium',
-      transition: 'all 0.2s',
-      opacity: 1,
-      _disabled: {
-        opacity: 0.4
-      }
-    },
-    errorText: {
-      color: mode('red.500', 'red.300')(props),
-      marginTop: 2,
-      fontSize: 'sm'
-    },
-    requiredIndicator: {
-      marginLeft: 1,
-      color: mode('red.500', 'red.300')(props)
-    },
-    helperText: {
-      marginTop: 2,
-      color: mode('gray.500', 'whiteAlpha.600')(props),
-      lineHeight: 'normal',
-      fontSize: 'sm'
-    },
-    errorIcon: {
-      marginRight: '0.5em',
-      color: mode('red.500', 'red.300')(props)
-    }
-  };
-};
-
-const Form = {
-  register,
-  baseStyle
-};
+const { Form } = theme.components;
 
 export default Form;
