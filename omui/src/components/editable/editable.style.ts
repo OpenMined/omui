b3fd252c94f707fd6ae941080939b74e60505d0c
Héricles <hericles.me@gmail.com>
2020-07-23 23:33:58 -0300

    theme refactoring: editable

--- omui/src/components/editable/editable.style.ts
+++ omui/src/components/editable/editable.style.ts
@@ -1,34 +1,29 @@
-// import { ComponentTheme } from '@chakra-ui/theme-tools';
-import { SystemProps } from '@chakra-ui/system';
+import { SystemStyleObject } from '@chakra-ui/system';
+import { BaseStyle } from '@chakra-ui/theme-tools';
 
-const base: SystemProps = {
-  fontSize: 'inherit',
-  fontWeight: 'inherit',
-  textAlign: 'inherit',
-  bg: 'transparent',
-  transition: 'all 0.2s',
+const register = {
+  parts: ['container', 'preview', 'input']
+} as const;
+
+const previewStyle: SystemStyleObject = {
   borderRadius: 'md',
-  paddingX: '3px',
-  marginX: '-3px'
+  paddingY: '3px',
+  transition: 'all 0.2s'
 };
 
-// TODO: remove the 'any' type
-const Editable: any = {
-  baseStyle: {
-    Preview: {
-      ...base,
-      cursor: 'text',
-      display: 'inline-block'
-    },
-    Input: {
-      ...base,
-      outline: 0,
-      width: 'full',
-      _placeholder: {
-        opacity: 0.6
-      }
-    }
+const baseStyle: BaseStyle<typeof register> = {
+  container: {},
+  preview: previewStyle,
+  input: {
+    ...previewStyle,
+    width: 'full',
+    _placeholder: { opacity: 0.6 }
   }
 };
 
-export default Editable;
+const editable = {
+  register,
+  baseStyle
+};
+
+export default editable;
