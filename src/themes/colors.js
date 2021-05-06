c04003a5cf1ba44212da1430f25971b64ff3396a
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-05-06 14:11:23 +0100

    feat: omui colors

---
+++ src/themes/colors.js
@@ -0,0 +1,15 @@
+export default {
+  gray: {
+    50: '#fff',
+    100: '#e3e2e9'
+  },
+  magenta: {},
+  orange: {},
+  marigold: {},
+  lime: {},
+  cyan: {},
+  blue: {},
+  violet: {},
+  purple: {},
+  red: {}
+}
