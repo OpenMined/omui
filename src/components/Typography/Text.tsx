915cc911d857dedce22ee9095763a71982febf40
Diego Kfuri <kkfuri@gmail.com>
2021-06-09 17:41:54 -0300

    feat: update typography atom
    *  Refactor component to match rules from design system
    *  Improve types and typescript annotations

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -2,66 +2,84 @@ import React from 'react'
 import cn from 'classnames'
 import type {PropsWithChildren} from 'react'
 
-type TextComponent = 'p' | 'span' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-type TextSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl'
-type FontWeight = 'font-medium' | 'font-normal' | 'font-black' | 'font-bold'
+export type TextAsType = 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+export type TextSizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl'
+
+interface Props {
+  /**
+   * The element of the text (default as `p`)
+   */
+  as?: TextAsType
+  /**
+   * The size of the text (default as `md`)
+   */
+  size?: TextSizeType
+  /**
+   * Defines if the text should be bold
+   */
+  bold?: boolean
+  className?: string
+}
+
+interface TextAsMonoProps {
+  /**
+   * Defines if the text should be mono faced
+   */
+  mono?: true
+  underline?: never
+  uppercase?: never
+}
+
+interface TextAsRegularProps {
+  mono?: false
+  /**
+   * Defines if the text should be underlined
+   */
+  underline?: boolean
+  /**
+   * Defines if the text should be uppercased
+   */
+  uppercase?: boolean
+}
+
+type DefaultProps<T> = PropsWithChildren<T & (TextAsMonoProps | TextAsRegularProps)>
+
+export type TextProps = DefaultProps<Props>
 
 export function Text({
-  as = 'h1',
+  as = 'p',
   size = 'md',
-  bold = false,
-  uppercase = false,
-  underline = false,
-  mono = false,
+  bold,
+  uppercase,
+  underline,
+  mono,
   className,
   children,
   ...props
-}: PropsWithChildren<{
-  as: TextComponent
-  size: TextSizes
-  bold: boolean
-  uppercase: boolean
-  underline: boolean
-  mono: boolean
-  className: string
-}>) {
-  const underlined = underline ? 'underline' : ''
+}: TextProps) {
+  const smallSized = React.useMemo(() => ['xs', 'sm', 'md', 'lg'].includes(size), [size])
 
-  let uppercased = uppercase ? 'uppercase' : ''
-
-  let fontSize = `text-${size}`
-
-  let fontFamily = ['xs', 'sm', 'md', 'lg'].includes(size) ? 'font-roboto' : 'font-rubik'
-
-  let fontWeight: FontWeight
-
-  if (fontFamily === 'font-roboto') {
-    fontWeight = bold ? 'font-bold' : 'font-normal'
-  } else {
-    fontWeight = bold ? 'font-black' : 'font-medium'
+  const underlined = {underline: underline && !mono}
+  const uppercased = {uppercase: uppercase || mono}
+  const fontFamily = {
+    'font-roboto': smallSized && !mono,
+    'font-rubik': !smallSized && !mono,
+    'font-firacode': mono
+  }
+  const fontWeight = {
+    'font-normal': smallSized && !bold,
+    'font-medium': !smallSized && !bold,
+    'font-bold': (smallSized && bold) || (mono && bold),
+    'font-black': !smallSized && bold && !mono
   }
 
-  if (mono) {
-    fontFamily = 'fira-code'
-    uppercased = 'uppercase'
-  }
+  const uppercaseCustomRule = uppercase && ['4xl', '5xl', '6xl'].includes(size) && !mono
+  const monoCustomRule = mono && ['4xl', '5xl'].includes(size)
 
-  if (mono && bold) {
-    fontWeight = 'font-bold'
-  }
-
-  if (uppercase && ['4xl', '5xl', '6xl'].includes(size)) {
-    if (size == '4xl') {
-      fontSize = 'text-4xl-upper'
-    } else if (size == '5xl') {
-      fontSize = 'text-5xl-upper'
-    } else {
-      fontSize = 'text-6xl-upper'
-    }
-  }
-
-  if (mono && ['4xl', '5xl'].includes(size)) {
-    fontSize = size == '4xl' ? 'text-4xl-mono' : 'text-5xl-mono'
+  const fontSize = {
+    [`text-${size}`]: !uppercaseCustomRule && !monoCustomRule,
+    [`text-${size}-upper`]: uppercaseCustomRule,
+    [`text-${size}-mono`]: monoCustomRule
   }
 
   const classes = cn(fontFamily, fontWeight, fontSize, underlined, uppercased, className)
@@ -69,26 +87,28 @@ export function Text({
   return React.createElement(as, {className: classes, ...props}, children)
 }
 
-export function H1(props) {
+export type HeadingProps = DefaultProps<Omit<Props, 'as' | 'size'>>
+
+export function H1(props: HeadingProps) {
   return <Text {...props} as="h1" size="5xl" />
 }
 
-export function H2(props) {
+export function H2(props: HeadingProps) {
   return <Text {...props} as="h2" size="4xl" />
 }
 
-export function H3(props) {
+export function H3(props: HeadingProps) {
   return <Text {...props} as="h3" size="3xl" />
 }
 
-export function H4(props) {
+export function H4(props: HeadingProps) {
   return <Text {...props} as="h4" size="2xl" />
 }
 
-export function H5(props) {
+export function H5(props: HeadingProps) {
   return <Text {...props} as="h5" size="xl" />
 }
 
-export function H6(props) {
+export function H6(props: HeadingProps) {
   return <Text {...props} as="h6" size="sm" />
 }
