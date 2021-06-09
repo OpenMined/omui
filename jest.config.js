e7266ba84b2cccac5cfe3611fe23a4a568d4bcac
Diego Kfuri <kkfuri@gmail.com>
2021-06-09 17:38:54 -0300

    chore(test): setup jest with typescript support

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.js']
+}
