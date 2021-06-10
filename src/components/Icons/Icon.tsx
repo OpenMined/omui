8e137f8eab2fa5665d1e58fef86c6c30082d52ff
Diego Kfuri <kkfuri@gmail.com>
2021-06-10 10:19:54 -0300

    feat(atom/icon): improve tsdoc and refactor style

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -1,83 +1,91 @@
 import React from 'react'
 import cn from 'classnames'
 
-type IconSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'responsive'
-type IconStyles = 'gray' | 'solid' | 'subtle' | 'outline' | 'ghost'
-type IconContainers = 'round' | 'square'
+type IconSizeProp = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'custom'
+type IconVariantProp = 'gray' | 'solid' | 'subtle' | 'outline' | 'ghost'
+type IconContainerProp = 'round' | 'square'
 
 export type IconProps = {
-  size?: IconSizes
-  style?: IconStyles
-  container?: IconContainers
-  icon: React.ReactType
+  /**
+   * The size of the icon.
+   * @defaultValue md
+   */
+  size?: IconSizeProp
+  /**
+   * The variant of the icon.
+   * @defaultValue solid
+   */
+  variant?: IconVariantProp
+  /**
+   * The container of the icon
+   * @defaultValue round
+   */
+  container?: IconContainerProp
+  icon: React.ElementType
+  containerProps?: React.ComponentPropsWithoutRef<'span'>
+  containerClassName?: string
   className?: string
 }
 
-const iconSizes = {
-  xs: 'w-3 h-3',
-  sm: 'w-3.5 h-3.5',
-  md: 'w-4 h-4',
-  lg: 'w-4.5 h-4.5',
-  xl: 'w-5 h-5'
+const VARIANTS = {
+  OUTLINE:
+    'bg-transparent shadow-icon-border border-primary-500 border dark:border-primary-200 text-primary-500 dark:text-primary-200',
+  GRAY: 'bg-gray-800 text-primary-200',
+  SOLID: 'bg-primary-500 text-white',
+  SUBTLE: 'bg-primary-200 text-primary-600',
+  GHOST: 'text-primary-500'
 }
 
-const containerStyles = {
-  xs: 'w-6 h-6',
-  sm: 'w-8 h-8',
-  md: 'w-9 h-9',
-  lg: 'w-11 h-11',
-  xl: 'w-12 h-12'
-}
-
-const borderRadiusStyles = {
-  round: 'rounded-full',
-  square: 'rounded-md'
-}
-
-function makeResponsive(styleObj) {
-  return Object.keys(styleObj).reduce((prev, curr) => {
-    const style = styleObj[curr]
-    const styleRules = style.split(' ').map((rule: string) => (curr === 'xs' ? rule : `${curr}:${rule}`))
-    return `${prev} ${styleRules.join(' ')}`
-  }, '')
-}
-
-export function Icon({size = 'md', style = 'solid', container = 'round', icon, className}: IconProps) {
-  let bg
-  let fill
-  let border
-
-  if (style === 'outline') {
-    bg = 'bg-transparent shadow-icon-border'
-    border = 'border-primary-500 border dark:border-primary-200'
-    fill = 'fill-current text-primary-500 dark:text-primary-200'
-  } else if (style === 'gray') {
-    bg = 'bg-gray-800'
-    fill = 'fill-current text-primary-200'
-  } else if (style === 'solid') {
-    bg = 'bg-primary-500'
-    fill = 'fill-current text-white'
-  } else if (style === 'subtle') {
-    bg = 'bg-primary-200'
-    fill = 'fill-current text-primary-600'
-  } else if (style === 'ghost') {
-    fill = 'fill-current'
+export function Icon({
+  size = 'md',
+  variant = 'solid',
+  container = 'round',
+  icon: Icon,
+  className,
+  containerClassName,
+  containerProps,
+  ...props
+}: IconProps) {
+  const variants = {
+    [VARIANTS.OUTLINE]: variant === 'outline',
+    [VARIANTS.GRAY]: variant === 'gray',
+    [VARIANTS.SOLID]: variant === 'solid',
+    [VARIANTS.SUBTLE]: variant === 'subtle',
+    [VARIANTS.GHOST]: variant === 'ghost'
+  }
+  const borderRadius = {
+    'rounded-full': container === 'round',
+    'rounded-md': container === 'square'
+  }
+  const sizes = {
+    container: {
+      'w-6 h-6': size === 'xs',
+      'w-8 h-8': size === 'sm',
+      'w-9 h-9': size === 'md',
+      'w-11 h-11': size === 'lg',
+      'w-12 h-12': size === 'xl'
+    },
+    icon: {
+      'w-3 h-3': size === 'xs',
+      'w-3.5 h-3.5': size === 'sm',
+      'w-4 h-4': size === 'md',
+      'w-4.5 h-4.5': size === 'lg',
+      'w-5 h-5': size === 'xl'
+    }
   }
 
-  const borderRadius = borderRadiusStyles[container]
-  const containerClasses = containerStyles[size]
-  const iconSize = iconSizes[size]
-  const classes = cn('flex items-center justify-center', bg, border, borderRadius, containerClasses, className)
-  const iconClasses = cn(iconSize, fill)
-  const IconComponent = icon
-
-  if (style === 'ghost') {
-    return <IconComponent className={iconClasses} />
-  }
+  const containerClassNames = cn(
+    'flex items-center justify-center',
+    variants,
+    borderRadius,
+    sizes.container,
+    containerClassName
+  )
+  const iconClassNames = cn(sizes.icon, className)
 
   return (
-    <div className={classes}>
-      <IconComponent className={iconClasses} />
-    </div>
+    <span className={containerClassNames} {...containerProps}>
+      <Icon className={iconClassNames} {...props} />
+    </span>
   )
 }
