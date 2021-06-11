84ca9ef1abbd4acea1a5fade1e7217f112ef3527
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-11 19:35:08 +0100

    feat: update Badge stories
    - Added a story containing all possible Badge cases

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Story, Meta} from '@storybook/react'
-
-import {Badge, BadgeProps} from './Badge'
+import {Badge} from './Badge'
+import type {BadgeProps, BadgeTypeProp, BadgeVariantProp} from './Badge'
 
 export default {
   title: 'Components/Badges',
@@ -10,4 +10,25 @@ export default {
 
 const Template: Story<BadgeProps> = args => <Badge {...args}>{args.variant}</Badge>
 
+const types: BadgeTypeProp[] = ['outline', 'subtle', 'solid']
+const variants: BadgeVariantProp[] = ['gray', 'primary', 'tertiary', 'quaternary', 'danger', 'success']
+
+const AllBadges = () => (
+  <div className="space-y-4">
+    {variants.map(variant => (
+      <div className="flex space-x-2">
+        {types.map(type => (
+          <div className="capitalize">
+            <Badge type={type} variant={variant}>
+              {variant}
+            </Badge>
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+)
+
 export const Default = Template.bind({})
+
+export const AllCasesLight = AllBadges.bind({})
