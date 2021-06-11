15fa4ef8aa92ed96a15ac603834b257e9ed6daef
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-11 16:34:10 +0100

    fix: set the right badge colors in tests

--- src/components/Badge/__test__/Badge.test.js
+++ src/components/Badge/__test__/Badge.test.js
@@ -19,27 +19,27 @@ cases(
     {
       name: 'Should have colors based on default variant: primary',
       params: {},
-      result: 'border-primary-600 text-primary-600 dark:border-primary-200 dark:text-primary-200'
+      result: 'border-primary-500 text-primary-600 dark:border-primary-200 dark:text-primary-200'
     },
     {
       name: 'Should have colors based on variant gray',
       params: {variant: 'gray'},
-      result: 'border-gray-600 text-gray-600 dark:border-gray-200 dark:text-gray-200'
+      result: 'border-gray-500 text-gray-600 dark:border-gray-200 dark:text-gray-200'
     },
     {
       name: 'Should have colors based on variant primary',
       params: {variant: 'primary'},
-      result: 'border-primary-600 text-primary-600 dark:border-primary-200 dark:text-primary-200'
+      result: 'border-primary-500 text-primary-600 dark:border-primary-200 dark:text-primary-200'
     },
     {
       name: 'Should have colors based on variant danger',
       params: {variant: 'danger'},
-      result: 'border-error-600 text-error-600 dark:border-error-200 dark:text-error-200'
+      result: 'border-error-500 text-error-600 dark:border-error-200 dark:text-error-200'
     },
     {
       name: 'Should have classes based on default type: outline',
       params: {},
-      result: 'border-primary-600 text-primary-600 dark:border-primary-200 dark:text-primary-200'
+      result: 'border-primary-500 text-primary-600 dark:border-primary-200 dark:text-primary-200'
     },
     {
       name: 'Should have classes based on type subtle',
@@ -54,7 +54,7 @@ cases(
     {
       name: 'Should have classes based on type solid following gray custom rule',
       params: {type: 'solid', variant: 'gray'},
-      result: 'border-gray-500 bg-gray-500 text-primary-200'
+      result: 'border-gray-800 bg-gray-800 text-primary-200'
     }
   ]
 )
