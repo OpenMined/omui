514bb3c907ccf0a3ce65f01abb7ebd7d9ee15683
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-13 22:00:54 +0100

    feat: add colors to Divider, always use hr
    - Took the chance to improve the component code as well
    - Replaced <div> to always output <hr>

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,28 +1,49 @@
-import React, {PropsWithChildren} from 'react'
+import React from 'react'
 import cn from 'classnames'
+import type {HTMLAttributes} from 'react'
 
-export type OrientationProp = 'horizontal' | 'vertical'
-
-export type DividerProps = PropsWithChildren<{
+export type DividerOrientation = 'horizontal' | 'vertical'
+export type DividerColor = 'light' | 'black' | 'dark'
+export type DividerProps = {
   /**
-   * Define the orientation of the divider
+   * Sets the divider orientation
    * @defaultValue horizontal
    */
-  orientation?: OrientationProp
+  orientation?: DividerOrientation
+  /**
+   * Sets the divider theme to black (gray/900), light (gray/200) or dark (gray/700)
+   * @defaultValue dark
+   */
+  color?: DividerColor
+  /**
+   * Classes that are passed to the hr element
+   */
   className?: string
-}>
+}
 
-export function Divider({orientation = 'horizontal', children, className, ...props}: DividerProps) {
-  const orientationStyle = {
-    'border-l h-full w-px': orientation === 'vertical',
-    'border-t w-full': orientation === 'horizontal'
-  }
-  const component = children ? 'div' : 'hr'
-  const classes = cn('border-gray-700 dark:border-gray-200', orientationStyle, className)
+type Themes = {
+  [k in DividerColor]: string | string[]
+}
+const dividerColorClasses: Themes = {
+  light: 'border-gray-200',
+  dark: 'border-gray-700',
+  black: 'border-gray-900'
+}
 
-  return React.createElement(
-    component,
-    {className: classes, 'aria-orientation': orientation, role: component !== 'hr' ? 'separator' : undefined, ...props},
-    children
-  )
+type Orientation = {
+  [k in DividerOrientation]: string | string[]
+}
+const orientationClasses: Orientation = {
+  horizontal: 'border-t w-full',
+  vertical: 'border-l h-full w-px'
+}
+
+export function Divider({
+  orientation = 'horizontal',
+  color = 'dark',
+  className,
+  ...props
+}: DividerProps & HTMLAttributes<HTMLHRElement>) {
+  const classes = cn(dividerColorClasses[color], orientationClasses[orientation], className)
+  return <hr aria-orientation={orientation} role="separator" className={classes} {...props} />
 }
