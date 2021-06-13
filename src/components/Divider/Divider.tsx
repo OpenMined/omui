24c51067eeeb0afae9e89dd07cd84756c6dc04cd
Diego Kfuri <kkfuri@gmail.com>
2021-06-13 01:30:32 -0300

    fix(components/divider): remove unnused props
    Now the role and component are automatically set based on children

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -9,32 +9,20 @@ export type DividerProps = PropsWithChildren<{
    * @defaultValue horizontal
    */
   orientation?: OrientationProp
-  /**
-   * Define the component element.
-   * @defaultValue hr
-   */
-  component?: string
-  /**
-   * Define the element role.
-   */
-  role?: string
   className?: string
 }>
 
-export function Divider({
-  orientation = 'horizontal',
-  children,
-  component = children ? 'div' : 'hr',
-  role = component !== 'hr' ? 'separator' : undefined,
-  className,
-  ...props
-}: DividerProps) {
+export function Divider({orientation = 'horizontal', children, className, ...props}: DividerProps) {
   const orientationStyle = {
     'border-l h-full w-px': orientation === 'vertical',
     'border-t w-full': orientation === 'horizontal'
   }
-
+  const component = children ? 'div' : 'hr'
   const classes = cn('border-gray-700 dark:border-gray-200', orientationStyle, className)
 
-  return React.createElement(component, {className: classes, 'aria-orientation': orientation, role, ...props}, children)
+  return React.createElement(
+    component,
+    {className: classes, 'aria-orientation': orientation, role: component !== 'hr' ? 'separator' : undefined, ...props},
+    children
+  )
 }
