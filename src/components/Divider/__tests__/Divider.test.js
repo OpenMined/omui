1d1cae6dd3bccd744249c02d271ae86d4e8cfb94
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-13 22:12:48 +0100

    test: color classes test

--- src/components/Divider/__tests__/Divider.test.js
+++ src/components/Divider/__tests__/Divider.test.js
@@ -4,59 +4,94 @@ import {axe} from 'jest-axe'
 
 import {Divider} from '../Divider'
 
-describe('accessibility', () => {
-  test('No accessibility violation caught', async () => {
-    const {container} = render(<Divider />)
-    const result = await axe(container)
-    expect(result).toHaveNoViolations()
+describe('Divider', () => {
+  describe('accessibility', () => {
+    test('No accessibility violation caught', async () => {
+      const {container} = render(<Divider />)
+      const result = await axe(container)
+      expect(result).toHaveNoViolations()
+    })
+
+    cases(
+      'accesibility:aria-orientation',
+      ({params, result}) => {
+        render(<Divider data-testid="test-id" {...params} />)
+        const dividerElement = screen.getByTestId('test-id')
+        expect(dividerElement).toHaveAttribute('aria-orientation', result)
+      },
+      [
+        {
+          name: 'Default props, divider aria-orientation is horizontal',
+          params: {},
+          result: 'horizontal'
+        },
+        {
+          name: 'Prop orientation="horizontal", then aria-orientation=horizontal',
+          params: {orientation: 'horizontal'},
+          result: 'horizontal'
+        },
+        {
+          name: 'Prop orientation="vertical", then aria-orientation=vertical',
+          params: {orientation: 'vertical'},
+          result: 'vertical'
+        }
+      ]
+    )
+
+    cases(
+      'accesibility:role',
+      ({params}) => {
+        render(<Divider data-testid="test-id" {...params} />)
+        const dividerElement = screen.getByTestId('test-id')
+        expect(dividerElement).toHaveAttribute('role', 'separator')
+      },
+      [
+        {
+          name: 'Default props',
+          params: {}
+        },
+        {
+          name: 'Prop orientation="horizontal"',
+          params: {orientation: 'horizontal'}
+        },
+        {
+          name: 'Prop orientation="vertical"',
+          params: {orientation: 'vertical'}
+        }
+      ]
+    )
   })
 
-  cases(
-    'accesibility:aria-orientation',
-    ({params, result}) => {
-      render(<Divider data-testid="test-id" {...params} />)
-      const dividerElement = screen.getByTestId('test-id')
-      expect(dividerElement).toHaveAttribute('aria-orientation', result)
-    },
-    [
-      {
-        name: 'Default props, divider aria-orientation is horizontal',
-        params: {},
-        result: 'horizontal'
+  describe('styles:classes', () => {
+    cases(
+      'styles:classes:color',
+      ({params, result}) => {
+        render(<Divider data-testid="test-id" {...params} />)
+        const dividerElement = screen.getByTestId('test-id')
+        expect(dividerElement).toHaveClass(result)
       },
-      {
-        name: 'Prop orientation="horizontal", then aria-orientation=horizontal',
-        params: {orientation: 'horizontal'},
-        result: 'horizontal'
-      },
-      {
-        name: 'Prop orientation="vertical", then aria-orientation=vertical',
-        params: {orientation: 'vertical'},
-        result: 'vertical'
-      }
-    ]
-  )
-
-  cases(
-    'accesibility:role',
-    ({params}) => {
-      render(<Divider data-testid="test-id" {...params} />)
-      const dividerElement = screen.getByTestId('test-id')
-      expect(dividerElement).toHaveAttribute('role', 'separator')
-    },
-    [
-      {
-        name: 'Default props',
-        params: {}
-      },
-      {
-        name: 'Prop orientation="horizontal", then aria-orientation=horizontal',
-        params: {orientation: 'horizontal'}
-      },
-      {
-        name: 'Prop orientation="vertical", then aria-orientation=vertical',
-        params: {orientation: 'vertical'}
-      }
-    ]
-  )
+      [
+        {
+          name: 'Default props',
+          params: {},
+          result: 'border-gray-700'
+        },
+        {
+          name: 'Prop color="dark"',
+          params: {color: 'dark'},
+          result: 'border-gray-700'
+        },
+        {
+          name: 'Prop color="black"',
+          params: {color: 'black'},
+          result: 'border-gray-900'
+        },
+        {
+          name: 'Prop color="light"',
+          params: {color: 'light'},
+          result: 'border-gray-200'
+        }
+      ]
+    )
+  })
 })
