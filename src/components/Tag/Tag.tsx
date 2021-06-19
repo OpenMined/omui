52697cdde76448b604cbc5091bff8e05b0aa3186
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-19 04:31:40 +0100

    feat: refactor Tag component
    - Created more types for Tags
      * TagTypeProp
      * TagVariantProp
      * TagIconSideProp
    - Renamed a few props to better match Figma
    - Moved const out of the component
    - Added more TSDoc to TagProps, started a doc for Tag
    - Used OmuiColors to limit the color choices
    - Adjusted tests to match the new changes

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,79 +1,124 @@
 import React from 'react'
 import cn from 'classnames'
-import type {ReactNode, HTMLAttributes, PropsWithChildren} from 'React'
+import type {ReactElement, MouseEventHandler, PropsWithChildren} from 'React'
+import type {OmuiColors} from '@/styles/colorType'
 
 export type TagSizeProp = 'sm' | 'md' | 'lg'
+export type TagTypeProp = 'round' | 'square'
+export type TagVariantProp = 'gray' | 'primary' | 'tertiary' | 'quaternary'
 
-interface Props extends HTMLAttributes<HTMLElement> {
+const VARIANT_COLORS: Record<TagVariantProp, Partial<OmuiColors>> = {
+  gray: 'gray',
+  primary: 'primary',
+  tertiary: 'violet',
+  quaternary: 'blue'
+} as const
+
+interface Props {
   /**
    * The variant of the Tag.
    * @defaultValue outline
    */
-  variant: keyof typeof colorByVariant
+  variant?: TagVariantProp
   /**
    * The size of the Tag.
    * @defaultValue sm
    */
-  size: TagSizeProp
+  size?: TagSizeProp
   /**
    * Defines if the tag should be disabled.
    * @defaultValue false
    */
   disabled?: boolean
   /**
-   * Defines if the tag should be pill shaped.
-   * @defaultValue false
+   * The style of the Tag. The round style is similar to the artist formerly known as pill.
+   * @defaultValue square
    */
-  pill?: boolean
+  tagType?: TagTypeProp
   /**
-   * Render a icon as complement of the Tag.
+   * Classes that are passed down to the main component (button or span).
    */
-  icon?: ReactNode
+  className?: string
   /**
-   * Defines the position of the icon.
+   * When an onClick function is set, the tag becomes a button
    */
-  iconSide?: 'left' | 'right'
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-export type TagProps = PropsWithChildren<Props>
-
-const colorByVariant = {
-  gray: 'gray',
-  primary: 'primary',
-  tertiary: 'violet',
-  quaternary: 'blue'
+interface PropsWithoutIcons extends Props {
+  icon: null | undefined
+  iconSide: never
 }
 
+export type TagIconSideProp = 'left' | 'right'
+
+interface PropsWithIcons extends Props {
+  /**
+   * Icon component to be rendered next to the Tag.
+   */
+  icon: ReactElement
+  /**
+   * Position of the icon, left or right of the inner tag text. It is not allowed to have both positions or multiple icons.
+   */
+  iconSide: TagIconSideProp
+}
+
+export type TagProps = PropsWithChildren<PropsWithoutIcons | PropsWithIcons>
+
+const tagTypeStyles: Record<TagTypeProp, string> = {
+  round: 'rounded-full',
+  square: 'rounded-sm'
+}
+
+const tagTextSize: Record<TagSizeProp, string> = {
+  sm: 'text-sm',
+  md: 'text-md',
+  lg: 'text-lg'
+}
+
+const tagBackgoundAndTextColor: Record<TagVariantProp, string> = Object.assign(
+  {},
+  ...(Object.keys(VARIANT_COLORS) as TagVariantProp[]).map(variant => {
+    if (variant === 'gray') {
+      return {gray: 'text-gray-600 bg-gray-100 hover:text-primary-200 hover:bg-gray-800'}
+    }
+    const color = VARIANT_COLORS[variant]
+    return {[variant]: `text-${color}-600 bg-${color}-100 hover:text-white hover:bg-${color}-500`}
+  })
+)
+
+const defaultClasses = 'inline-flex items-center px-2.5 py-1 transition transition-colors'
+const defaultIconClasses = 'w-4 h-4'
+
+/**
+ * Tags are meant to indicate categories and typically links out to filtering mechanism.
+ */
 const Tag = ({
   variant = 'primary',
   size = 'md',
+  tagType = 'square',
+  disabled,
   icon: Icon,
   iconSide,
-  disabled,
-  pill = false,
-  onClick,
   className,
   children,
   ...props
 }: TagProps) => {
-  const clickable = !!onClick
-  const color = colorByVariant[variant]
+  const clickable = !!props.onClick
+  const Component: keyof JSX.IntrinsicElements = clickable ? 'button' : 'span'
   const classes = cn(
-    'inline-flex items-center px-2.5 py-1 transition transition-colors',
-    `text-${size}`,
-    `bg-${color}-100 text-${color}-600`,
-    color === 'gray' ? 'hover:text-primary-200 hover:bg-gray-800' : `hover:text-white hover:bg-${color}-500`,
-    disabled ? 'pointer-events-none opacity-50' : clickable && 'cursor-pointer',
-    pill ? 'rounded-full' : 'rounded-sm',
+    defaultClasses,
+    tagTextSize[size],
+    tagBackgoundAndTextColor[variant],
+    tagTypeStyles[tagType],
+    clickable && 'cursor-pointer',
+    disabled && 'pointer-events-none opacity-50',
     className
   )
-  const iconClasses = cn('w-4 h-4', {'ml-1.5': iconSide === 'right', 'mr-1.5': iconSide === 'left'})
-
-  const Component: keyof JSX.IntrinsicElements = clickable ? 'button' : 'span'
+  const iconClasses = cn(defaultIconClasses, iconSide === 'right' && 'ml-1.5', iconSide === 'left' && 'mr-1.5')
 
   return (
-    <Component onClick={onClick} {...props} className={classes}>
+    <Component {...props} className={classes}>
       {Icon && iconSide === 'left' ? <span className={iconClasses}>{Icon}</span> : null}
       {children}
       {Icon && iconSide === 'right' ? <span className={iconClasses}>{Icon}</span> : null}
--- src/components/Tag/__tests__/Tag.test.js
+++ src/components/Tag/__tests__/Tag.test.js
@@ -4,102 +4,109 @@ import cases from 'jest-in-case'
 
 import {Tag} from '../Tag'
 
-cases(
-  'Validate tag classes',
-  ({params, result}) => {
-    render(
-      <Tag data-testid="test-id" {...params}>
-        Omui
-      </Tag>
-    )
+describe('Tag', () => {
+  cases(
+    'styles:classes',
+    ({params, result}) => {
+      render(
+        <Tag data-testid="test-id" {...params}>
+          Omui
+        </Tag>
+      )
 
-    const tagElement = screen.getByTestId('test-id')
-    expect(tagElement).toHaveClass(result)
-  },
-  [
-    {
-      name: 'Should be default with md size, not pill style and primary colors',
-      params: {},
-      result: 'bg-primary-100 text-primary-600 rounded-sm text-md'
+      const tagElement = screen.getByTestId('test-id')
+      expect(tagElement).toHaveClass(result)
     },
-    {
-      name: 'Should have disabled classes',
-      params: {disabled: true},
-      result: 'opacity-50 pointer-events-none'
+    [
+      {
+        name: 'Should be default with md size, square and have primary colors',
+        params: {},
+        result: 'bg-primary-100 text-primary-600 rounded-sm text-md'
+      },
+      {
+        name: 'gray tag has different classes',
+        params: {variant: 'gray'},
+        result: 'bg-gray-100 text-gray-600 hover:bg-gray-800 hover:text-primary-200'
+      },
+      {
+        name: 'Should have disabled classes',
+        params: {disabled: true},
+        result: 'opacity-50 pointer-events-none'
+      },
+      {
+        name: 'Should have cursor pointer when clickable',
+        params: {onClick: jest.fn},
+        result: 'cursor-pointer'
+      },
+      {
+        name: 'Should have text size matching size',
+        params: {size: 'lg'},
+        result: 'text-lg'
+      },
+      {
+        name: 'Should be fully rounded when tagType is round',
+        params: {tagType: 'round'},
+        result: 'rounded-full'
+      }
+    ]
+  )
+
+  cases(
+    'render:icons',
+    ({params, result}) => {
+      render(
+        <Tag data-testid="test-id" {...params}>
+          Omui
+        </Tag>
+      )
+
+      const tagElement = screen.getByTestId('test-id')
+      expect(tagElement.innerHTML).toBe(result)
     },
-    {
-      name: 'Should have cursor pointer when clickable',
-      params: {onClick: jest.fn},
-      result: 'cursor-pointer'
-    },
-    {
-      name: 'Should have text size matching size',
-      params: {size: 'lg'},
-      result: 'text-lg'
-    },
-    {
-      name: 'Should be full rounded when pill shaped',
-      params: {pill: true},
-      result: 'rounded-full'
-    }
-  ]
-)
+    [
+      {
+        name: 'Should render icon on left side',
+        params: {icon: <i data-testid="icon" />, iconSide: 'left'},
+        result: '<span class="w-4 h-4 mr-1.5"><i data-testid="icon"></i></span>Omui'
+      },
+      {
+        name: 'Should render icon on right side',
+        params: {icon: <i data-testid="icon" />, iconSide: 'right'},
+        result: 'Omui<span class="w-4 h-4 ml-1.5"><i data-testid="icon"></i></span>'
+      }
+    ]
+  )
 
-cases(
-  'Render icon on defined side',
-  ({params, result}) => {
-    render(
-      <Tag data-testid="test-id" {...params}>
-        Omui
-      </Tag>
-    )
+  describe('props', () => {
+    test('Renders a button element', () => {
+      render(
+        <Tag data-testid="test-id" onClick={jest.fn}>
+          Omui
+        </Tag>
+      )
 
-    const tagElement = screen.getByTestId('test-id')
-    expect(tagElement.innerHTML).toBe(result)
-  },
-  [
-    {
-      name: 'Should render icon on left side',
-      params: {icon: <i data-testid="icon" />, iconSide: 'left'},
-      result: '<span class="w-4 h-4 mr-1.5"><i data-testid="icon"></i></span>Omui'
-    },
-    {
-      name: 'Should render icon on right side',
-      params: {icon: <i data-testid="icon" />, iconSide: 'right'},
-      result: 'Omui<span class="w-4 h-4 ml-1.5"><i data-testid="icon"></i></span>'
-    }
-  ]
-)
+      const tagElement = screen.getByTestId('test-id')
+      expect(tagElement.tagName).toBe('BUTTON')
+    })
 
-describe('Render the correct element based on props', () => {
-  test('Render a button element', () => {
-    render(
-      <Tag data-testid="test-id" onClick={jest.fn}>
-        Omui
-      </Tag>
-    )
+    test('Renders a span element', () => {
+      render(<Tag data-testid="test-id">Omui</Tag>)
 
-    const tagElement = screen.getByTestId('test-id')
-    expect(tagElement.tagName).toBe('BUTTON')
-  })
+      const tagElement = screen.getByTestId('test-id')
+      expect(tagElement.tagName).toBe('SPAN')
+    })
 
-  test('Render a span element', () => {
-    render(<Tag data-testid="test-id">Omui</Tag>)
+    test('Triggers a click on clickable element', () => {
+      const mockFn = jest.fn()
+      render(
+        <Tag data-testid="test-id" onClick={mockFn}>
+          Omui
+        </Tag>
+      )
 
-    const tagElement = screen.getByTestId('test-id')
-    expect(tagElement.tagName).toBe('SPAN')
-  })
-
-  test('Trigger click on clickable element', () => {
-    const mockFn = jest.fn()
-    render(
-      <Tag data-testid="test-id" onClick={mockFn}>
-        Omui
-      </Tag>
-    )
-
-    const tagElement = screen.getByTestId('test-id')
-    userEvent.click(tagElement)
-    expect(mockFn).toHaveBeenCalledTimes(1)
+      const tagElement = screen.getByTestId('test-id')
+      userEvent.click(tagElement)
+      expect(mockFn).toHaveBeenCalledTimes(1)
+    })
   })
 })
