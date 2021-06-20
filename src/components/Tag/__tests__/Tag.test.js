14f2c073a60de4da1f4a01f473917489fcd81fa6
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-20 16:26:56 +0100

    test: improve icon position testing for Tags

--- src/components/Tag/__tests__/Tag.test.js
+++ src/components/Tag/__tests__/Tag.test.js
@@ -1,8 +1,17 @@
-import {render, screen} from '@testing-library/react'
+import {render, screen, within, getNodeText, getByTestId} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import cases from 'jest-in-case'
 
 import {Tag} from '../Tag'
+
+const RandomIcon = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" data-testid="test-icon" {...props}>
+    <path
+      fill="currentColor"
+      d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm80 168c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm-160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm194.8 170.2C334.3 380.4 292.5 400 248 400s-86.3-19.6-114.8-53.8c-13.6-16.3 11-36.7 24.6-20.5 22.4 26.9 55.2 42.2 90.2 42.2s67.8-15.4 90.2-42.2c13.4-16.2 38.1 4.2 24.6 20.5z"
+    />
+  </svg>
+)
 
 describe('Tag', () => {
   cases(
@@ -19,9 +28,9 @@ describe('Tag', () => {
     },
     [
       {
-        name: 'Should be default with md size, square and have primary colors',
+        name: 'default should be square and have primary colors',
         params: {},
-        result: 'bg-primary-100 text-primary-600 rounded-sm text-md'
+        result: 'bg-primary-100 text-primary-600 rounded-sm'
       },
       {
         name: 'gray tag has different classes',
@@ -29,56 +38,108 @@ describe('Tag', () => {
         result: 'bg-gray-100 text-gray-600 hover:bg-gray-800 hover:text-primary-200'
       },
       {
-        name: 'Should have disabled classes',
+        name: 'can be disabled',
         params: {disabled: true},
         result: 'opacity-50 pointer-events-none'
       },
       {
-        name: 'Should have cursor pointer when clickable',
+        name: 'the cursor is pointer when the tag is clickable',
         params: {onClick: jest.fn},
         result: 'cursor-pointer'
       },
       {
-        name: 'Should have text size matching size',
-        params: {size: 'lg'},
-        result: 'text-lg'
-      },
-      {
-        name: 'Should be fully rounded when tagType is round',
+        name: 'the tag is round when tagType is round',
         params: {tagType: 'round'},
         result: 'rounded-full'
       }
     ]
-  )
+  ),
+    cases(
+      'styles:typography',
+      ({params, result}) => {
+        render(
+          <Tag data-testid="test-id" {...params}>
+            Omui
+          </Tag>
+        )
 
-  cases(
-    'render:icons',
-    ({params, result}) => {
-      render(
-        <Tag data-testid="test-id" {...params}>
-          Omui
-        </Tag>
-      )
-
-      const tagElement = screen.getByTestId('test-id')
-      expect(tagElement.innerHTML).toBe(result)
-    },
-    [
-      {
-        name: 'Should render icon on left side',
-        params: {icon: <i data-testid="icon" />, iconSide: 'left'},
-        result: '<span class="w-4 h-4 mr-1.5"><i data-testid="icon"></i></span>Omui'
+        const tagElement = screen.getByTestId('test-id')
+        const textElement = tagElement.querySelector('span')
+        expect(textElement).toHaveClass(result)
       },
-      {
-        name: 'Should render icon on right side',
-        params: {icon: <i data-testid="icon" />, iconSide: 'right'},
-        result: 'Omui<span class="w-4 h-4 ml-1.5"><i data-testid="icon"></i></span>'
-      }
-    ]
-  )
+      [
+        {
+          name: 'default text size should be md',
+          params: {},
+          result: 'text-md'
+        },
+        {
+          name: 'size=md produces a tag with text size equal to text-md',
+          params: {size: 'md'},
+          result: 'text-md'
+        },
+
+        {
+          name: 'size=lg produces a tag with text size equal to text-lg',
+          params: {size: 'lg'},
+          result: 'text-lg'
+        },
+        {
+          name: 'size=sm produces a tag with text size equal to text-sm',
+          params: {size: 'sm'},
+          result: 'text-sm'
+        }
+      ]
+    ),
+    cases(
+      'render:icons',
+      ({params}) => {
+        const testString = 'OMui'
+
+        render(
+          <Tag data-testid="test-id" {...params}>
+            {testString}
+          </Tag>
+        )
+
+        const tagElement = screen.getByTestId('test-id')
+
+        const randomIconElement = within(tagElement).getByTestId('test-icon')
+        expect(randomIconElement).toBeTruthy()
+
+        const tagSpanElements = tagElement.querySelectorAll('span')
+
+        if (params.iconSide === 'left') {
+          expect(getByTestId(tagSpanElements[0], 'test-icon')).toBeTruthy()
+          expect(getNodeText(tagSpanElements[0])).not.toMatch(testString)
+          expect(getNodeText(tagSpanElements[1])).toMatch(testString)
+        } else if (params.iconSide === 'right') {
+          expect(getNodeText(tagSpanElements[0])).toMatch(testString)
+          expect(getNodeText(tagSpanElements[1])).not.toMatch(testString)
+          expect(getByTestId(tagSpanElements[1], 'test-icon')).toBeTruthy()
+        }
+      },
+      [
+        {
+          name: 'Should render icon on left side',
+          params: {icon: RandomIcon, iconSide: 'left'}
+        },
+        {
+          name: 'Should render icon on right side',
+          params: {icon: RandomIcon, iconSide: 'right'}
+        }
+      ]
+    )
 
   describe('props', () => {
-    test('Renders a button element', () => {
+    test('default tag is a span element', () => {
+      render(<Tag data-testid="test-id">Omui</Tag>)
+
+      const tagElement = screen.getByTestId('test-id')
+      expect(tagElement.tagName).toBe('SPAN')
+    })
+
+    test('the tag is a button when onClick is set', () => {
       render(
         <Tag data-testid="test-id" onClick={jest.fn}>
           Omui
@@ -89,14 +150,7 @@ describe('Tag', () => {
       expect(tagElement.tagName).toBe('BUTTON')
     })
 
-    test('Renders a span element', () => {
-      render(<Tag data-testid="test-id">Omui</Tag>)
-
-      const tagElement = screen.getByTestId('test-id')
-      expect(tagElement.tagName).toBe('SPAN')
-    })
-
-    test('Triggers a click on clickable element', () => {
+    test('clicking on a tag works', () => {
       const mockFn = jest.fn()
       render(
         <Tag data-testid="test-id" onClick={mockFn}>
@@ -104,6 +158,7 @@ describe('Tag', () => {
         </Tag>
       )
 
+      expect(mockFn).toHaveBeenCalledTimes(0)
       const tagElement = screen.getByTestId('test-id')
       userEvent.click(tagElement)
       expect(mockFn).toHaveBeenCalledTimes(1)
