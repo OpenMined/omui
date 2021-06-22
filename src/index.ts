6b0cd8b3ef7765b496639689e1170acbf7228a76
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-06-22 19:50:45 +0100

    feat(components/list): export from index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ export {Button} from '@/components/Button/Button'
 export {Icon} from '@/components/Icon/Icon'
 export {Image} from '@/components/Image/Image'
 export {Badge} from '@/components/Badge/Badge'
+export {
+  List,
+  OrderedList,
+  UnorderedList,
+  ListItem,
+  ListAvatarItem,
+  ListProgressItem,
+  ListIconItem,
+  ListContainedItem
+} from '@/components/List/List'
